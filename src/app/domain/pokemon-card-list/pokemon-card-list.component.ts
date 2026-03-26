d0697ed72b8c2f686f58255e97e7ac3ef0c9e265
Corentin Jéré <corentinjere@gmail.com>
2026-03-26 22:48:37 +0100

    Feature: Create pokemon-card-list component.

---
+++ src/app/domain/pokemon-card-list/pokemon-card-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card-list',
+  imports: [],
+  templateUrl: './pokemon-card-list.component.html',
+  styleUrl: './pokemon-card-list.component.scss'
+})
+export class PokemonCardListComponent {
+
+}
